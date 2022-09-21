65f12ec50d43f875afaa5d6d79e42cdf946ae3e7
RSashaQA <a.romanov.limehd@gmail.com>
2022-09-22 00:08:13 +0300

    login2check fix test bug

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,7 +1,7 @@
 pipeline {
   agent any
   stages {
-    stage('prepare playwright') {
+    stage('prepare workspace') {
       steps {
         sh '''
           rm -rf $WORKSPACE/allure-results
@@ -13,10 +13,26 @@ pipeline {
         '''
       }
     }
+    stage('account manage') {
+      steps {
+      sh '''
+        curl --location --request POST 'https://pl.iptv2021.com/api/v4/login' \
+        --header 'User-Agent: {"platform":"web","app":"limehd.tv"}' \
+        --header 'Cookie: advanced-frontend=83ef94f5f9d0c5d316d8634c85ef3cb7' \
+        --form 'email="testdeleteme@test.test"' \
+        --form 'password="qqqqqq"'
+      '''
+      sh '''
+      curl --location --request GET 'https://pl.iptv2021.com/api/v1/account-delete' \
+      --header 'User-Agent: {"platform":"web","app":"limehd.tv"}' \
+      --header 'Cookie: advanced-frontend=83ef94f5f9d0c5d316d8634c85ef3cb7'
+      '''
+      }
+    }
     stage('test') {
       steps {
         sh '''
-          npx playwright test --reporter=line,experimental-allure-playwright --workers 8
+          npx playwright test login2check.spec.js --reporter=line,experimental-allure-playwright --workers 8
         '''
         } 
       }
--- tests/login2check.spec.js
+++ tests/login2check.spec.js
@@ -1,55 +1,47 @@
-// const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * max);
-//   }
+test.setTimeout(120000)
 
-//   test.setTimeout(120000)
+test('Тест двойной авторизации в разные аккаунты', async ({ page }) => {
 
-// test('Тест двойной авторизации в разные аккаунты', async ({ page }) => {
+const userName1 = "STPLIME999@gmail.com"
+const userName2 = "TEST777@gmail.com"
+const userPassword1 = "qqqqqq"
 
-// const userName1 = "STPLIME999@gmail.com"
-// const userName2 = "TEST777@gmail.com"
-// const userPassword1 = "qqqqqq"
+    await page.goto('https://limehd.tv/login');
 
-//     await page.goto('https://limehd.tv/login');
+    //авторизуемся в учетную запись STPLIME999@gmail.com
+    await page.locator('[placeholder="Введите e-mail"]').fill(userName1);
+    await page.locator('input[type="password"]').fill(userPassword1);
+    await page.locator('text=Войти').click();
 
-//     //авторизуемся в учетную запись STPLIME999@gmail.com
-//     await page.locator('[placeholder="Введите e-mail"]').fill(userName1);
-//     await page.locator('input[type="password"]').fill(userPassword1);
-//     await page.locator('text=Войти').click();
+    //переходим в профиль
+    await page.locator('header >> text=s').click({clickCount:2});
+    await page.locator('text=Общая информация').click();
 
-//     await page.waitForTimeout(6000)
+    //сверяем учетную запись в профиле
+    const userNameLoginCheck1 = await page.innerText('div > .default__layout > .page-main > .profile__title-container > .profile__title');
+    expect(userNameLoginCheck1).toBe(userName1);
 
-//     //переходим в профиль
-//     await page.locator('header >> text=s').click({clickCount:2});
-//     await page.locator('text=Общая информация').click();
+    //выходим из учетной записи
+    await page.locator('header >> text=s').click({clickCount:2});
+    await page.locator('text=Выйти из аккаунта').first().click();
 
-//     //сверяем учетную запись в профиле
-//     const userNameLoginCheck1 = await page.innerText('div > .default__layout > .page-main > .profile__title-container > .profile__title');
-//     expect(userNameLoginCheck1).toBe(userName1);
+    //авторизаяция второй учетной записи
+    await page.goto('https://limehd.tv/login');
 
-//     //выходим из учетной записи
-//     await page.locator('header >> text=s').click({clickCount:2});
-//     await page.locator('text=Выйти из аккаунта').first().click();
+    //авторизуемся в учетную запись TEST777@gmail.com
+    await page.locator('[placeholder="Введите e-mail"]').fill(userName2);
+    await page.locator('input[type="password"]').fill(userPassword1);
+    await page.locator('text=Войти').click();
 
-//     //авторизаяция за вторую учетную запись
-//     await page.goto('https://limehd.tv/login');
+    //переходим в профиль
+    await page.locator('header >> text=t').click({clickCount:2});
+    await page.locator('text=Общая информация').click();
 
-//     //авторизуемся в учетную запись TEST777@gmail.com
-//     await page.locator('[placeholder="Введите e-mail"]').fill(userName2);
-//     await page.locator('input[type="password"]').fill(userPassword1);
-//     await page.locator('text=Войти').click();
-
-//     await page.waitForTimeout(6000)
-
-//     //переходим в профиль
-//     await page.locator('header >> text=s').click({clickCount:2});
-//     await page.locator('text=Общая информация').click();
-
-//     try{
-//     //сверяем учетную запись в профиле
-//     const userNameLoginCheck2 = await page.innerText('div > .default__layout > .page-main > .profile__title-container > .profile__title');
-//     expect(userNameLoginCheck2).toBe(userName2);
-//     } catch (err) {console.log('bug https://limehd.atlassian.net/browse/PW-302')}
-// })
+    // try{
+    //сверяем учетную запись в профиле
+    const userNameLoginCheck2 = await page.innerText('div > .default__layout > .page-main > .profile__title-container > .profile__title');
+    expect(userNameLoginCheck2).toBe(userName2);
+    // } catch (err) {console.log('bug https://limehd.atlassian.net/browse/PW-302')}
+})
