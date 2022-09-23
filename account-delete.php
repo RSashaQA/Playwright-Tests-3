9d2ba7d3755a9ef729e14f8aefc710d6a91eece3
RSashaQA <a.romanov.limehd@gmail.com>
2022-09-23 17:35:20 +0300

    php 2

--- account-delete.php
+++ account-delete.php
@@ -3,21 +3,15 @@
 $headers = [
     "User-Agent" => '{"sdk":25,"version_name":"4.4.0","version_code":647,"platform":"android","device_id":"123123123","name":"YOUR_DEVICE_NAME","app":"com.infolink.limeiptv"} '
 ];
-
 $server = "https://pl.iptv2021.com";
-
 $curl = curl_init();
-
 $response = curl_exec($curl);
-
 $accs = [
     [
         "email" => "testdeleteme@test.test",
         "pass"  => "qqqqqq",
     ]
 ];
-
-
 
 foreach ($accs as $acc) {
 
@@ -31,7 +25,7 @@ foreach ($accs as $acc) {
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => array($acc),
-        CURLOPT_HTTPHEADER => ($headers),
+        CURLOPT_HTTPHEADER => array($headers),
     ));
       
     if ($response["success"] && isset($response["token"])) {
@@ -51,7 +45,7 @@ foreach ($accs as $acc) {
             CURLOPT_HTTPHEADER => array_merge($headers, ["X-Token" => $response["token"]])
           ));
 
-        if ($response) {
+        if ($response["success"]) {
             print "Account deleting successful\n";
             print_r($response);
             print "\n";
