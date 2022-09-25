aab692155fd00c81435a89e4fa3f2403dc11bba1
RSashaQA <a.romanov.limehd@gmail.com>
2022-09-25 18:22:27 +0300

    qwe

--- account-delete.php
+++ account-delete.php
@@ -19,18 +19,29 @@ $accs = [
 ];
 
 foreach ($accs as $acc) {
-    $response = \user\bin\Curl::init("$server/api/v1/login", $acc, "POST", "json", $headers);
-    if (\user\bin\Curl::code() == 200 && $response && $response["success"] && isset($response["token"])) {
-        print "Login successful\n";
-        print_r($response);
-        print "\n";
+   
+      $ch = curl_init($server.'api/v1/login');
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($acc, '', '&'));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($ch, CURLOPT_HEADER, $headers);
+      $html = curl_exec($ch);
+      curl_close($ch);
+    echo $html;
 
-        $response = \user\bin\Curl::init("$server/api/v1/account-delete", [], "GET", "json", array_merge($headers, ["X-Token" => $response["token"]]));
+    // $response = \user\bin\Curl::init("$server/api/v1/login", $acc, "POST", "json", $headers);
+    // if (\user\bin\Curl::code() == 200 && $response && $response["success"] && isset($response["token"])) {
+    //     print "Login successful\n";
+    //     print_r($response);
+    //     print "\n";
 
-        if (\user\bin\Curl::code() == 200 && $response) {
-            print "Account deleting successful\n";
-            print_r($response);
-            print "\n";
-        }
-    }
+    //     $response = \user\bin\Curl::init("$server/api/v1/account-delete", [], "GET", "json", array_merge($headers, ["X-Token" => $response["token"]]));
+
+    //     if (\user\bin\Curl::code() == 200 && $response) {
+    //         print "Account deleting successful\n";
+    //         print_r($response);
+    //         print "\n";
+    //     }
+    // }
 }
