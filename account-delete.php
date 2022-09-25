a0e0fc27ca45dcac0cb997570b17ff571b4d0cf6
RSashaQA <a.romanov.limehd@gmail.com>
2022-09-25 20:55:17 +0300

    account delete

--- account-delete.php
+++ account-delete.php
@@ -1,47 +1,51 @@
 
 <?php 
 
-$headers = [
-    "User-Agent" => '{"sdk":25,"version_name":"4.4.0","version_code":647,"platform":"android","device_id":"123123123","name":"YOUR_DEVICE_NAME","app":"com.infolink.limeiptv"}'
-];
+$useragent = '{"sdk":25,"version_name":"4.4.0","version_code":647,"platform":"android","device_id":"123123123","name":"YOUR_DEVICE_NAME","app":"com.infolink.limeiptv"}';
 
 $server = "https://pl.iptv2021.com";
 
 $accs = [
     [
-        'email' => 'testdeleteme@test.test',
-        'pass'  => 'qqqqqq',
-    ],
-    [
-        'email' => 'sdkfsdgklj2@mail.com',
-        'pass'  => 'sdfhsdklj',
+        'email' => 'testdeleteme224053043@limehd.tv',
+        'password'  => 'wwwwww',
     ],
 ];
 
 foreach ($accs as $acc) {
-   
-      $ch = curl_init($server.'api/v1/login');
+      $ch = curl_init($server.'/api/v1/login');
       curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($acc, '', '&'));
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($acc));
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($ch, CURLOPT_HEADER, $headers);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+      curl_setopt($ch, CURLOPT_HEADER, 1);
+      curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
       $html = curl_exec($ch);
+      if ($html !== false)
+{
+  $ch_info = curl_getinfo($ch);
+  $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  $header = substr($html, 0, $ch_info['header_size']);
+  $html = substr($html, $ch_info['header_size']);
+} 
+$data = json_decode($html, true);
       curl_close($ch);
-    var_dump($html);
-
-    // $response = \user\bin\Curl::init("$server/api/v1/login", $acc, "POST", "json", $headers);
-    // if (\user\bin\Curl::code() == 200 && $response && $response["success"] && isset($response["token"])) {
-    //     print "Login successful\n";
-    //     print_r($response);
-    //     print "\n";
-
-    //     $response = \user\bin\Curl::init("$server/api/v1/account-delete", [], "GET", "json", array_merge($headers, ["X-Token" => $response["token"]]));
-
-    //     if (\user\bin\Curl::code() == 200 && $response) {
-    //         print "Account deleting successful\n";
-    //         print_r($response);
-    //         print "\n";
-    //     }
-    // }
+    
+      if ($http_code == 200 && $data['token']) {
+        $ch_delete = curl_init($server.'/api/v1/account-delete');
+        curl_setopt($ch_delete, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch_delete, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch_delete, CURLOPT_HTTPHEADER, ['X-Token: '. $data['token']]);
+        curl_setopt($ch_delete, CURLOPT_USERAGENT, $useragent);
+        $html = curl_exec($ch_delete);
+        $httpcode = curl_getinfo($ch_delete, CURLINFO_HTTP_CODE);
+        print_r($html);
+        curl_close($ch_delete);
+//         if ($httpcode == 200) {
+//         print "Account deleting successful\n";
+//             print_r($html);
+//             print "\n";
+// }
+}
 }
