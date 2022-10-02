162a85372cb40164a6f57b00a9d45361756a23c1
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-02 18:51:27 +0300

    q

--- account-delete.php
+++ account-delete.php
@@ -57,6 +57,90 @@ $accs = [
         'email' => 'testdeletemRegChro@test.test',
         'password'  => 'qqqqqq',
     ],
+    [
+        'email' => 'testdeleteme396142637@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme779656790@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme101775382@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme75748793@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme236998534@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme20442749@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme201584630@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme614659077@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme586018778@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme154623175@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme84196314@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme576611359@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme309444991@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme180160209@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme703039528@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme694355023@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme107327003@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme382257388@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme394402204@test.test',
+        'password' => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme695755178@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeleteme289925346@limehd.tv',
+        'password' => 'qqqqqq',
+    ],
 ];
 
 
