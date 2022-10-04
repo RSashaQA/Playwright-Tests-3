aa4bac3188c730b26b98974b5a69e61b621db0f2
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-04 14:45:24 +0300

    test

--- account-delete.php
+++ account-delete.php
@@ -18,6 +18,18 @@ $accs = [
         'password'  => 'qqqqqq',
     ],
     [
+        'email' => 'testdeletemewebkitfav1@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeletemechromiumfav1@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
+        'email' => 'testdeletemefirefoxfav1@test.test',
+        'password'  => 'qqqqqq',
+    ],
+    [
         'email' => 'testdeleteme@test.test',
         'password'  => 'qqqqqq',
     ],
