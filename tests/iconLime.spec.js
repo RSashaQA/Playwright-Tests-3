49d389d270c0f6f73f2d358cc15ae66ee569ff80
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-04 09:37:23 +0300

    icon Lime

---
+++ tests/iconLime.spec.js
@@ -0,0 +1,17 @@
+const { test, expect } = require('@playwright/test');
+
+test.use({ viewport: { width: 1920, height: 1080 } });
+test.setTimeout(10000);
+
+
+test('Тест нажать на лого Lime - переход на главную сраницу', async ({ page }) => {
+
+    const response = await page.goto('https://limehd.tv/movies');
+    if (response.status() > 399) {
+        throw new Error(`Failed with response code ${response.status()}`)
+    }
+
+    await page.locator('page-logo').first().click();
+    await page.waitForURL('https://limehd.tv/tv')
+    await expect(page.locator('text=Смотрите онлайн трансляцию федеральных телеканалов на сайте Лайм HD TV круглосуточно и бесплатно')).toBeVisible();
+});
