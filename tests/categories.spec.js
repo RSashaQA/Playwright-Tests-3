cccbad13aff0bb20a36fea12d6e5c1b31127a6e0
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-24 12:58:07 +0300

    фикс смены категории, добавлен таймаут

--- tests/categories.spec.js
+++ tests/categories.spec.js
@@ -13,7 +13,7 @@ test('Тест переключение между категориями кан
 
     await page.locator('text=Популярные').click();
     await expect(page.locator('text=Россия 1')).toBeVisible({timeout:10000});
-    await expect(page.locator('text=Первый канал')).toBeVisible({timeout:10000});
+    await expect(page.locator('text=Первый канал')).toBeVisible();
     await page.waitForTimeout(1000);
 
     await page.locator('text=Все').first().click();
@@ -22,19 +22,22 @@ test('Тест переключение между категориями кан
     await page.waitForTimeout(1000);
 
     await page.locator('text=Региональные').first().click();
-    await expect(page.locator('text=VOSTOK')).toBeVisible();
+    await expect(page.locator('text=VOSTOK')).toBeVisible({timeout:10000});
     await expect(page.locator('text=БСТ (УФА)')).toBeVisible(); 
     await expect(page.locator('text=Россия 1')).toBeHidden();
     await expect(page.locator('text=Первый канал')).toBeHidden();
-
     await page.waitForTimeout(1000);
 
-
-
+    await page.locator('text=Развлечения').first().click();
+    await expect(page.locator('text=ОХОТА И РЫБАЛКА')).toBeVisible({timeout:10000});
+    await expect(page.locator('text=УСАДЬБА')).toBeVisible(); 
+    await expect(page.locator('text=Россия 1')).toBeHidden();
+    await expect(page.locator('text=Первый канал')).toBeHidden();
     await page.waitForTimeout(1000);
+
 
     await page.locator('text=Спорт').first().click();
-    await expect(page.locator('text=Матч! Страна')).toBeVisible();
+    await expect(page.locator('text=Матч! Страна')).toBeVisible({timeout:10000});
     await expect(page.locator('text=Матч! Боец')).toBeVisible(); 
     await expect(page.locator('text=Россия 1')).toBeHidden();
     await expect(page.locator('text=Первый канал')).toBeHidden();
@@ -42,7 +45,7 @@ test('Тест переключение между категориями кан
     await page.waitForTimeout(1000);
 
     await page.locator('text=Детям').first().click();
-    await expect(page.locator('text=Канал DISNEY')).toBeVisible();
+    await expect(page.locator('text=Канал DISNEY')).toBeVisible({timeout:10000});
     await expect(page.locator('text=МУЛЬТИЛАНДИЯ')).toBeVisible(); 
     await expect(page.locator('text=Россия 1')).toBeHidden();
     await expect(page.locator('text=Первый канал')).toBeHidden();
@@ -50,7 +53,7 @@ test('Тест переключение между категориями кан
     await page.waitForTimeout(1000);
 
     await page.locator('text=Музыка').first().click();
-    await expect(page.locator('text=VOSTOK')).toBeVisible();
+    await expect(page.locator('text=VOSTOK')).toBeVisible({timeout:10000});
     await expect(page.locator('text=МУЗСОЮЗ')).toBeVisible(); 
     await expect(page.locator('text=Россия 1')).toBeHidden();
     await expect(page.locator('text=Первый канал')).toBeHidden();
@@ -58,7 +61,7 @@ test('Тест переключение между категориями кан
     await page.waitForTimeout(1000);
 
     await page.locator('text=Новости >> nth=0').first().click();
-    await expect(page.locator('text=КРИК ТВ')).toBeVisible();
+    await expect(page.locator('text=КРИК ТВ')).toBeVisible({timeout:10000});
     await expect(page.locator('text=ИЗВЕСТИЯ')).toBeVisible(); 
     await expect(page.locator('text=МУЗСОЮЗ')).toBeHidden();
     await expect(page.locator('text=VOSTOK')).toBeHidden();
@@ -66,7 +69,7 @@ test('Тест переключение между категориями кан
     await page.waitForTimeout(1000);
 
     await page.locator('text=Познавательные').first().click();
-    await expect(page.locator('text=Здоровое ТВ')).toBeVisible();
+    await expect(page.locator('text=Здоровое ТВ')).toBeVisible({timeout:10000});
     await expect(page.locator('text=ZOOПАРК')).toBeVisible(); 
     await expect(page.locator('text=МУЗСОЮЗ')).toBeHidden();
     await expect(page.locator('text=VOSTOK')).toBeHidden();
@@ -74,7 +77,7 @@ test('Тест переключение между категориями кан
     await page.waitForTimeout(1000);
 
     await page.locator('text=Фильмы').first().click();
-    await expect(page.locator('text=Киносезон')).toBeVisible();
+    await expect(page.locator('text=Киносезон')).toBeVisible({timeout:10000});
     await expect(page.locator('text=Киносерия')).toBeVisible(); 
     await expect(page.locator('text=МУЗСОЮЗ')).toBeHidden();
     await expect(page.locator('text=VOSTOK')).toBeHidden();
