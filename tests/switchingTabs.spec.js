7d29924283fea59c997957742bff506854af5804
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-26 15:50:16 +0300

    Фикс теста "смена вкладок"

--- tests/switchingTabs.spec.js
+++ tests/switchingTabs.spec.js
@@ -4,52 +4,47 @@ const { limehdUrl } = require('./consts/constants');
 test.use({ viewport: { width: 1920, height: 1080 } });
 
 test('Тест смена вкладок, ТВ-каналы', async ({ page }) => {
-
     //нажимаем на вкладку ТВ-каналы, прорверяем, что страница доступна
-    const response = await page.goto(limehdUrl, { waitUntil: "commit" });
+    const response = await page.goto(limehdUrl);
     if (response.status() > 399) {
         throw new Error(`Failed with response code ${response.status()}`)
     }
+    // await page.waitForTimeout(8000);
+    await page.locator('span:has-text("ТВ-каналы")').click()
     //видны каналы
-    await expect(page.locator('text=Первый канал')).toBeVisible();
-    await expect(page.locator('text=ТЕЛЕКОМПАНИЯ')).toBeVisible();
-    await expect(page.locator('text=ПЕТЕРБУРГ - 5 КАНАЛ')).toBeVisible();
+    await page.waitForTimeout(8000);
+    await expect(page.locator('h3:has-text("Первый канал")')).toBeVisible({ timeout: 3000 });
+    await expect(page.locator('h3:has-text("ТЕЛЕКОМПАНИЯ")')).toBeVisible({ timeout: 3000 });
+    await expect(page.locator('h3:has-text("ПЕТЕРБУРГ - 5 КАНАЛ")')).toBeVisible({ timeout: 3000 });
 });
 
 test('Тест смена вкладок, Кино', async ({ page, browserName }) => {
 
     //прорверяем, что страница VOD доступна
-    const response = await page.goto(limehdUrl + '/movies', { waitUntil: "commit" });
+    const response = await page.goto(limehdUrl);
     if (response.status() > 399) {
         throw new Error(`Failed with response code ${response.status()}`)
     }
-
-
+    // await page.waitForTimeout(8000);
+    await page.locator('span:has-text("Кино")').click()
     if (browserName == 'webkit') {
     }
     if (browserName == 'chromium') {
-        await expect(page.locator('text=Популярное')).toBeVisible();
+        await expect(page.locator('.movies')).toBeVisible();
     }
     if (browserName == 'firefox') {
-        await expect(page.locator('text=Популярное')).toBeVisible();
+        await expect(page.locator('.movies')).toBeVisible();
     }
-
 });
-
 
 test('Тест смена вкладок, Подписки', async ({ page }) => {
 
     //прорверяем, что страница Подписок доступна
-    const response = await page.goto(limehdUrl + '/subscribes', { waitUntil: "commit" });
+    const response = await page.goto(limehdUrl);
     if (response.status() > 399) {
         throw new Error(`Failed with response code ${response.status()}`)
     }
-
-    await expect(page.locator('text=START')).toBeVisible();
-
-    // //проверяем, что на странице есть текст "о подписках"
-    // const packsPage1 = await page.innerText('div > .default__layout > .packs__container > .form__apps > p:nth-child(1)');
-    // expect(packsPage1).toBe('Выберите и оплатите подписку.');
-    // const packsPage2 = await page.innerText('div > .default__layout > .packs__container > .form__apps > p:nth-child(2)');
-    // expect(packsPage2).toBe('Авторизуйтесь в приложении или на смарт-тв для просмотра на всех устройствах');
+    // await page.waitForTimeout(8000);
+    await page.locator('header ul >> text=Подписки').click()
+    await expect(page.locator('.packs__list')).toBeVisible();
 });
