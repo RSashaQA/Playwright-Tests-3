c591b22546c0e16cf71c7b3f251a773c6f369de1
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-26 08:59:00 +0300

    icon test переделал тест

--- tests/iconLime.spec.js
+++ tests/iconLime.spec.js
@@ -1,16 +1,18 @@
 const { test, expect } = require('@playwright/test');
 const { limehdUrl } = require('./consts/constants');
 
-
-test.use({ viewport: { width: 1920, height: 1080 } });
-
 test('Тест нажать на лого Lime - переход на главную сраницу', async ({ page }) => {
 
     const response = await page.goto(limehdUrl + '/subscribes');
     if (response.status() > 399) {
         throw new Error(`Failed with response code ${response.status()}`)
     }
+    //Переходим категорию "подписки"
+    await page.locator('header ul >> text=Подписки').click();
+    await expect(page.locator('.packs__list')).toBeVisible();
+    await page.waitForTimeout(2000);
 
+    //Нажимаем на лого "Лайм" - ожидаем переход на главную страницу
     await page.locator('.page-logo').first().click();
-    await expect(page.locator('text=Смотрите онлайн трансляцию федеральных телеканалов на сайте Лайм HD TV круглосуточно и бесплатно')).toBeVisible();
+    await expect(page.locator('.channel__list-container')).toBeVisible();
 });
