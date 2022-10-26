d17d03fb546e8f14908e9d10dc694d965099264f
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-26 11:31:46 +0300

    переделан скрипт fav.spec.js

--- tests/fav.spec.js
+++ tests/fav.spec.js
@@ -2,88 +2,93 @@ const { test, expect } = require('@playwright/test');
 const { limehdUrl } = require('./consts/constants');
 
 test.use({ viewport: { width: 1920, height: 1080 } });
+test.setTimeout(100000);
 
 test('Тест добавление/удаление каналов из страницы канала без авторизации', async ({ page }) => {
 
-    await page.goto(limehdUrl);
+  const response = await page.goto(limehdUrl);
+  if (response.status() > 399) {
+      throw new Error(`Failed with response code ${response.status()}`)
+  }
+    //await page.goto(limehdUrl);
 
     //добавляем НТВ в избранные, через страницу канала
     await page.locator('text=Телекомпания «НТВ»').click();
-    await page.waitForTimeout(1000)
-    await page.locator('.page-main > .stream__item > .stream__title-container > .stream__favorite-container > .stream__favorite').click()
-    await page.waitForTimeout(1000)
-    await page.locator('text=Все телеканалы').click();
-    await page.waitForTimeout(1000)
+    await page.waitForTimeout(1000);
+    await page.locator('.stream__favorite-container').click()
+    await page.waitForTimeout(1000);
+    await page.locator('.login__button-arrow').click();
+    await page.waitForTimeout(1000);
 
     //добавляем ТВ - ЦЕНТР в избранные, через страницу канала
     await page.locator('text=ТВ ЦЕНТР - Москва').click();
-    await page.waitForTimeout(1000)
-    await page.locator('.page-main > .stream__item > .stream__title-container > .stream__favorite-container > .stream__favorite').click()
-    await page.waitForTimeout(1000)
-    await page.locator('text=Все телеканалы').click();
-    await page.waitForTimeout(1000)
+    await page.waitForTimeout(1000);
+    await page.locator('.stream__favorite-container').click()
+    await page.waitForTimeout(1000);
+    await page.locator('.login__button-arrow').click();
+    await page.waitForTimeout(1000);
 
     //проверяем, что каналы в избранном
-    await page.locator('text=Избранные').click();
-    await page.isVisible('text=НТВ', 'text=ЦЕНТР')
+    await page.locator('.favorite__icon').click();
+    await expect(page.locator('text=Телекомпания «НТВ»')).toBeVisible();
+    await expect(page.locator('text=ТВ ЦЕНТР - Москва')).toBeVisible();
+    //done
 
     //удаляем НТВ из избранных, через страницу канала
     await page.locator('text=Телекомпания «НТВ»').click();
-    await page.waitForTimeout(2000)
-    await page.locator('#__layout > div > div.default__layout > div > div.stream__item > div.stream__title-container > div.stream__favorite-container > img').click()
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1000);
+    await page.locator('.stream__favorite-container').click()
+    await page.waitForTimeout(1000);
     await page.locator('.login__button-arrow').click();
-    await page.locator('text=Избранные').click();
+    await page.waitForTimeout(1000);
+    await page.locator('.favorite__icon').click();
+    await page.waitForTimeout(2000);
 
-    //удаляем ТВ ЦЕНТР - Москва из избранных, через страницу канала
+    //удаляем "ТВ ЦЕНТР - Москва" из избранных, через страницу канала
     await page.locator('text=ТВ ЦЕНТР - Москва').click();
-    await page.waitForTimeout(2000)
-    await page.locator('#__layout > div > div.default__layout > div > div.stream__item > div.stream__title-container > div.stream__favorite-container > img').click();
-    await page.waitForTimeout(2000)
-    await page.locator('header a:has-text("ТВ-каналы")').click();
-
-    await page.waitForTimeout(1000)
-    await page.locator('text=Избранные').click();
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(2000);
+    await page.locator('.stream__favorite-container').click();
+    await page.waitForTimeout(2000);
+    await page.locator('.login__button-arrow').click();
+    await page.waitForTimeout(5000);
+    await page.locator('.favorite__icon').click();
 
     //проверяем, что избранном есть надпись 'Добавьте каналы в избранное'
-    expect(page.locator('text=Добавьте каналы в избранное')).toBeVisible({timeout:10000});
+    await page.waitForTimeout(5000);
+    expect(page.locator('text=Телекомпания «НТВ»')).toBeHidden();
+    expect(page.locator('text=ТВ ЦЕНТР - Москва')).toBeHidden();
+    expect(page.locator('text=Добавьте каналы в избранное')).toBeVisible();
 });
 
 
 test('Тест добавление/удаление каналов из страницы избранных каналов без авторизации', async ({ page }) => {
 
-  await page.goto(limehdUrl);
 
-  //добавляем НТВ в избранные, через страницу канала
-  await page.locator('text=Телекомпания «НТВ»').click();
-  await page.waitForTimeout(1000)
-  await page.waitForSelector('.page-main > .stream__item > .stream__title-container > .stream__favorite-container > .stream__favorite')
-  await page.click('.page-main > .stream__item > .stream__title-container > .stream__favorite-container > .stream__favorite')
-  await page.waitForTimeout(1000)
-  await page.locator('text=Все телеканалы').click();
-  await page.waitForTimeout(1000)
+  const response = await page.goto(limehdUrl);
+  if (response.status() > 399) {
+    throw new Error(`Failed with response code ${response.status()}`)
+  }
 
-  //добавляем ТВ ЦЕНТР - Москва в избранные, через страницу канала
-  await page.locator('text=ТВ ЦЕНТР - Москва').click();
-  await page.waitForTimeout(1000)
-  await page.waitForSelector('.page-main > .stream__item > .stream__title-container > .stream__favorite-container > .stream__favorite')
-  await page.click('.page-main > .stream__item > .stream__title-container > .stream__favorite-container > .stream__favorite')
-  await page.waitForTimeout(1000)
-  await page.locator('text=Все телеканалы').click();
-  await page.waitForTimeout(1000)
+  //добавляем каналы в избранное из плейлиста
+  let i = 0;
+  do {
+    i++;
+    await page.locator('li.channel__item-container:nth-child(' + i + ') > div:nth-child(1) > a:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)').click();
+    await page.waitForTimeout(500);
+  } while (i < 3);
 
-  //проверяем, что каналы в избранном
-  await page.locator('text=Избранные').click();
-  await page.isVisible('text=НТВ', 'text=ЦЕНТР')
+  //переходим в "Избранное"
+  await page.locator('.favorite__icon').click();
+  await page.waitForTimeout(3000);
 
-  //удаляем НТВ и ТВ ЦЕНТР - Москва из избранных, через страницу канала
-  await page.waitForSelector('.channel__item-container:nth-child(2) > .channel-container > .channel__item > .channel__wrapper > .channel__title-container > .channel__title-wrapper > .channel__favorite-container > .channel__favorite')
-  await page.click('.channel__item-container:nth-child(2) > .channel-container > .channel__item > .channel__wrapper > .channel__title-container > .channel__title-wrapper > .channel__favorite-container > .channel__favorite')
-  
-  await page.waitForSelector('.channel__wrapper > .channel__title-container > .channel__title-wrapper > .channel__favorite-container > .channel__favorite')
-  await page.click('.channel__wrapper > .channel__title-container > .channel__title-wrapper > .channel__favorite-container > .channel__favorite')
-  await page.waitForTimeout(2000)
+  //удаляем каналы из избранного находясь в плейлисте
+  let b = 0; 
+  do {
+    b++;
+    await page.locator('.channel__favorite-container').first().click();
+    await page.waitForTimeout(500);
+    await console.log(b);
+  } while (b < 3);
 
-  expect(page.locator('text=Добавьте каналы в избранное')).toBeVisible({timeout:10000});
+  expect(page.locator('text=Добавьте каналы в избранное')).toBeVisible();
 });
