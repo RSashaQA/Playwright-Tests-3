808ef8b75aaa3f32cda3007063b291497e4dc9eb
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-26 16:46:27 +0300

    EPG fix

--- tests/EPG20test.spec.js
+++ tests/EPG20test.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { limehdUrl } = require('./consts/constants');
 const programmTime = '.page-main > .stream__item > .stream__current-program > .current-program__title-container > .current-program__time';
-const noProgramName = '.current-program__title';
+const noProgramName = 'text=Телепрограмма отсутствует';
 
 test.use({ viewport: { width: 1920, height: 1080 } });
 
@@ -11,7 +11,7 @@ test('проверка наличия ЕПГ на Первом канале', as
 
     await page.goto(limehdUrl + '/channel/1kanal');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -19,7 +19,7 @@ test('проверка наличия ЕПГ на Россия 1', async ({ page
 
     await page.goto(limehdUrl + '/channel/rossia1');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -27,7 +27,7 @@ test('проверка наличия ЕПГ на Матч!', async ({ page }) =
 
     await page.goto(limehdUrl + '/channel/match');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -35,7 +35,7 @@ test('проверка наличия ЕПГ на НТВ', async ({ page }) => {
 
     await page.goto(limehdUrl + '/channel/ntv');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -43,7 +43,7 @@ test('проверка наличия ЕПГ на 5 канале', async ({ page
 
     await page.goto(limehdUrl + '/channel/5kanal');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -51,7 +51,7 @@ test('проверка наличия ЕПГ на Россия К', async ({ pag
 
     await page.goto(limehdUrl + '/channel/russiak');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -59,7 +59,7 @@ test('проверка наличия ЕПГ на Россия 24', async ({ pag
 
     await page.goto(limehdUrl + '/channel/rossia24');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -67,7 +67,7 @@ test('проверка наличия ЕПГ на Карусель', async ({ pa
 
     await page.goto(limehdUrl + '/channel/karusel');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -75,7 +75,7 @@ test('проверка наличия ЕПГ на ОТР', async ({ page }) => {
 
     await page.goto(limehdUrl + '/channel/otr');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -83,7 +83,7 @@ test('проверка наличия ЕПГ на ТВЦ', async ({ page }) => {
 
     await page.goto(limehdUrl + '/channel/tvc');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -91,7 +91,7 @@ test('проверка наличия ЕПГ на РЕН ТВ', async ({ page })
 
     await page.goto(limehdUrl + '/channel/rentv');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -99,7 +99,7 @@ test('проверка наличия ЕПГ на Спас', async ({ page }) =>
 
     await page.goto(limehdUrl + '/channel/spas');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -107,7 +107,7 @@ test('проверка наличия ЕПГ на СТС', async ({ page }) => {
 
     await page.goto(limehdUrl + '/channel/sts');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -115,7 +115,7 @@ test('проверка наличия ЕПГ на Домашний', async ({ pa
 
     await page.goto(limehdUrl + '/channel/domashniy');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -123,7 +123,7 @@ test('проверка наличия ЕПГ на ТВ-3', async ({ page }) => {
 
     await page.goto(limehdUrl + '/channel/tv3');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -131,7 +131,7 @@ test('проверка наличия ЕПГ на Пятница', async ({ page
 
     await page.goto(limehdUrl + '/channel/pyatnica');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -139,7 +139,7 @@ test('проверка наличия ЕПГ на Звезда', async ({ page }
 
     await page.goto(limehdUrl + '/channel/zvezda');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -147,7 +147,7 @@ test('проверка наличия ЕПГ на МИР', async ({ page }) => {
 
     await page.goto(limehdUrl + '/channel/mir');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -155,14 +155,14 @@ test('проверка наличия ЕПГ на ТНТ', async ({ page }) => {
 
     await page.goto(limehdUrl + '/channel/tnt');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
 test('проверка наличия ЕПГ на МУЗ-ТВ', async ({ page }) => {
 
     await page.goto(limehdUrl + '/channel/muztv');
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
 
@@ -170,6 +170,6 @@ test('проверка наличия ЕПГ на МИР-24', async ({ page }) =
 
     await page.goto(limehdUrl + '/channel/mir24');
 
-    await expect(page.locator(noProgramName)).not.toContainText("Телепрограмма отсутствует");
+    await expect(page.locator(noProgramName)).toBeHidden();
     await expect(page.locator(programmTime)).toBeVisible();
 })
