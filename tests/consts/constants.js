bb8604ffc3317b7182a9d2ec0d5c6093c64c60db
RSashaQA <a.romanov.limehd@gmail.com>
2022-11-03 07:16:58 +0300

    module.exports.

--- tests/consts/constants.js
+++ tests/consts/constants.js
@@ -1,1 +1,37 @@
 module.exports.limehdUrl = "https://limehd.tv";
+module.exports.pervKanal = 'Первый канал';
+module.exports.rossia1 = 'Россия-1';
+module.exports.match = 'Матч ТВ';
+module.exports.ntv = 'Телекомпания НТВ';
+module.exports.pyatKanal = 'Петербург — 5 канал';
+module.exports.rossiak = 'Россия — К';
+module.exports.rossia24 = 'Россия — 24';
+module.exports.karusel = 'Детско-юношеский телеканал «Карусель»';
+module.exports.otr = 'Общественное телевиденье России';
+module.exports.tvc = 'ТВ ЦЕНТР — Москва';
+module.exports.rentv = 'РЕН ТВ';
+module.exports.spas = 'Спас'
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
+module.exports.
